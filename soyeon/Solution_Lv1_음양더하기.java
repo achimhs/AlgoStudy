528e6f80b27e0c83505ce9a95af9c66bf5395569
Soyeon Park <thdusdm6391@naver.com>
2022-07-20 10:00:51 +0900

    7/20알고리즘

---
+++ soyeon/Solution_Lv1_음양더하기.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Solution_Lv1_음양더하기 {
+	/*
+	 * String의 길이는 length() / 배열의 길이는 length
+	 */
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer = 0;
+		for (int i = 0; i < absolutes.length; i++) {
+			if (signs[i] == true) {
+				answer += absolutes[i];
+			} else {
+				answer -= absolutes[i];
+			}
+			//answer += absolutes[i] * (signs[i]? 1: -1);
+		}
+		return answer;
+	}
+}
