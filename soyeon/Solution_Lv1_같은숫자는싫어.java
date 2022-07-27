8d1f6b78ee7c6f5e5c2427a0fe8250a780c30f7d
Soyeon Park <thdusdm6391@naver.com>
2022-07-27 10:12:06 +0900

    7/27알고리즘

--- soyeon/Solution_Lv1_같은숫자는싫어.java
+++ soyeon/Solution_Lv1_같은숫자는싫어.java
@@ -1,5 +1,27 @@
 package programmers;
 
-public class Solution_Lv1_같은숫자는싫어 {
+import java.util.Stack;
 
+public class Solution_Lv1_같은숫자는싫어 {
+	/*
+	 * 틀린 이유1: 스택이 비어있을 때 처리를  stack.peek()!=arr[i] 이것보다 먼저 해줘야 함
+	 * 틀린 이유2: 스택에서 pop을 하고 배열에 넣을 때 i--부분(차라리 get(i)가 나을지도)
+	 * 틀린 이유3: answer의 배열의 크기를 다시 설정해줘야 함
+	 * 알게된 점: push(i), pop(), peek(), isEmpty(), get(i)도 쓰인다!!
+	 */
+	public int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (stack.size() == 0 || stack.peek() != arr[i]) {
+				stack.push(arr[i]);
+			}
+		}
+
+		int[] answer = new int[stack.size()];
+		for (int i = stack.size() - 1; i >= 0; i--) {
+			answer[i] = stack.pop();
+		}
+		return answer;
+	}
 }
--- soyeon/Solution_Lv1_폰켓몬.java
+++ soyeon/Solution_Lv1_폰켓몬.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class Solution_Lv1_폰켓몬 {
 	/*
 	 * size와 max의 크기를 비교해서 출력하는 것까지 생각 못함 
+	 * 함수체크
 	 */
 	public int solution(int[] nums) {
 		int max = nums.length / 2;
