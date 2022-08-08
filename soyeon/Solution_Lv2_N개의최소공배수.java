2c33d53a86480551258297a8b4f22ac215405305
Soyeon Park <thdusdm6391@naver.com>
2022-08-08 22:32:26 +0900

    8/8알고리즘

---
+++ soyeon/Solution_Lv2_N개의최소공배수.java
@@ -0,0 +1,28 @@
+package programmers;
+
+public class Solution_Lv2_N개의최소공배수 {
+	public int solution(int[] arr) {
+		int answer = 0;
+		
+		if (arr.length == 1)
+			return arr[0]; // 원소가 한 개인 경우 바로 출력
+
+		int g = gcd(arr[0], arr[1]); // 처음 두 원소의 최대공약수
+		answer = (arr[0] * arr[1]) / g; // 처음 두 원소의 최소공배수
+
+		if (arr.length > 2) {
+			for (int i = 2; i < arr.length; i++) {
+				g = gcd(answer, arr[i]);
+				answer = (answer * arr[i]) / g;
+			}
+		}
+
+		return answer;
+	}
+	static int gcd(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		}
+		return gcd(b, a % b);
+	}
+}
